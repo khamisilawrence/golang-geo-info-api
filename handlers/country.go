0f567a6043d2bd90d8d1069c1399dd4e9a37c301
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2024-11-18 22:23:18 -0500

    modify the handler to use the new utility function that interacts with external APIs

--- handlers/country.go
+++ handlers/country.go
@@ -1,13 +1,15 @@
 package handlers
 
 import (
-	"github.com/labstack/echo/v4"
+	"geo-info-api/utils"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 // GetCountryInfo handles the /info route
 func GetCountryInfo(c echo.Context) error {
-	// Extract 'country' from the query string
+	// Extract 'country' from query parameter
 	country := c.QueryParam("country")
 	if country == "" {
 		return c.JSON(http.StatusBadRequest, map[string]string{
@@ -15,10 +17,24 @@ func GetCountryInfo(c echo.Context) error {
 		})
 	}
 
-	// Placeholder response (will be replaced with API integration)
+	// Fetch country data from Restcountries API
+	countryData, err := utils.FetchCountryData(country)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"error": err.Error(),
+		})
+	}
+
+	// Build response
 	response := map[string]interface{}{
-		"country": country,
-		"message": "Country information will be available soon",
+		"country":       countryData["name"].(map[string]interface{})["common"],
+		"capital":       countryData["capital"].([]interface{})[0],
+		"population":    countryData["population"],
+		"area_size_km2": countryData["area"],
+		"languages":     countryData["languages"],
+		"timezone":      countryData["timezones"],
+		"currency":      countryData["currencies"],
+		"flag":          countryData["flags"].(map[string]interface{})["png"],
 	}
 
 	return c.JSON(http.StatusOK, response)
