2412936e529ce995eb472dc265d6e3fcf1ca4cd3
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2024-11-18 22:30:31 -0500

    update `GetCountryInfo` to use the response formatter
    - modify the handler to use the success formatter

--- handlers/country.go
+++ handlers/country.go
@@ -37,5 +37,6 @@ func GetCountryInfo(c echo.Context) error {
 		"flag":          countryData["flags"].(map[string]interface{})["png"],
 	}
 
-	return c.JSON(http.StatusOK, response)
+	return utils.HandleSuccess(c, response)
+
 }
